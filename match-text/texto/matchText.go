f151083ddd97e7625da313ff1c4de660b33959da
bosca <boscardinvinicius@gmail.com>
2019-06-14 14:44:07 -0300

    .

--- match-text/texto/matchText.go
+++ match-text/texto/matchText.go
@@ -11,28 +11,6 @@ func (t Texto) MatchText() []byte {
 	match := regexp.MustCompile("^[a-zA-Z]+$").MatchString(t.Texto)
 	fmt.Println(match)
 
-	// r, _ := regexp.Compile("p([a-z]+)ch")
-	// fmt.Println(r.MatchString("peach"))
-	// fmt.Println(r.FindString("peach punch"))
-	// fmt.Println(r.FindStringIndex("peach punch"))
-	// fmt.Println(r.FindStringSubmatch("peach punch"))
-	// fmt.Println(r.FindStringSubmatchIndex("peach punch"))
-	// fmt.Println(r.FindAllString("peach punch pinch", -1))
-	// fmt.Println(r.FindAllStringSubmatchIndex(
-	// 	"peach punch pinch", -1))
-	// fmt.Println(r.FindAllString("peach punch pinch", 2))
-	// fmt.Println(r.Match([]byte("peach")))
-	// r = regexp.MustCompile("p([a-z]+)ch")
-	// fmt.Println(r)
-	// fmt.Println(r.ReplaceAllString("a peach", "<fruit>"))
-	// in := []byte("a peach")
-	// out := r.ReplaceAllFunc(in, bytes.ToUpper)
-	// fmt.Println(string(out))
-	// jsonResp := simplejson.New()
-	// jsonResp.Set("certo", t)
-
-	// payload, _ := jsonResp.MarshalJSON()
-	// json.Marshal(t)
 	retorno := Retorno{
 		t.Texto,
 		match,
